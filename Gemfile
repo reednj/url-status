aea8032c5d3dc2240237c560757032bd26bcec7b
Nathan Reed <reednj@gmail.com>
2016-10-28 14:15:12 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+group :development do
+    gem 'test-unit'
+end
+
+ruby '>1.9.2'
+gem 'rest-client', '~>1.8'
---
+++ app.rb
@@ -0,0 +1,12 @@
+#
+# Get the status of a list of web pages
+# Nathan Reed, 2016-10-28 
+#
+
+class App
+    def main
+        puts 'Hello, World!'
+    end
+end
+
+App.new.main
